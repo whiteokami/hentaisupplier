459587a1a09eb4836adf8f8d282cb16a001497e6
Suretic <37001138+Suretic@users.noreply.github.com>
2019-05-11 22:50:53 +0200

    info command

---
+++ commands/info.js
@@ -0,0 +1,19 @@
+const fu = require("../modules/futils.js");
+const Discord = require("discord.js");
+module.exports = {
+  name: "info",
+  description: "shows info about the bot",
+  execute(msg,args,client,msgc) {
+    var d = client.user.createdAt.toString().split(" ");
+    const eminfo = new Discord.RichEmbed()
+    .setColor("0x"+fu.rhex())
+    .setTitle("information about the bot")
+    .addField("source", "https://github.com/suretic/hentaisupplier")
+    .addField("creation date", `${d[1]} ${d[2]} ${d[3]}`)
+    .addField("servers", `${client.guilds.size}`)
+    .addField("received messages since last restart", msgc)
+    .addBlankField()
+    .setFooter("Want this bot on your server? → ask suretic#5364", client.user.avatarURL)
+    msg.channel.send(eminfo);
+  },
+};
